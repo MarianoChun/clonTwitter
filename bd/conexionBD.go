afdc462c3495bee8895b2e214729741405340923
Mariano Chun <marianochun01@gmail.com>
2021-12-24 13:35:02 -0300

    Add obtencion de keys mediante viper

--- bd/conexionBD.go
+++ bd/conexionBD.go
@@ -6,13 +6,30 @@ import (
 	"context"
 	"log"
 
-	"github.com/MarianoChun/clonTwitter/keys"
+	"github.com/spf13/viper"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+// Obtener KEY
+func viperEnvVariable(key string) string {
+	// Configuramos el path del config file
+	viper.SetConfigFile(".env")
+	// Leemos el archivo configurado como config file
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Fatalf("Error al leer el config file %s", err)
+	}
+
+	value, ok := viper.Get(key).(string)
+	if !ok {
+		log.Fatalf("Error al convertir la key a string")
+	}
+	return value
+}
+
 var MongoCN = ConectarBD() // Aqui se ejecuta la conexion a la base de datos, devuelve la conexion en si misma
-var URI string = "mongodb+srv://Mariano:" + keys.MONGO_TOKEN + "@twitter.psjyi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+var URI string = "mongodb+srv://Mariano:" + viperEnvVariable("MONGO_TOKEN") + "@twitter.psjyi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 var clientOptions = options.Client().ApplyURI(URI)
 
 /* ConectarBD es la funcion que me permite conectar la base de datos */
